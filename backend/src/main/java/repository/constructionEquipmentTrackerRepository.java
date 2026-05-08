4208914a4caf4d9de4914dfedac1c88d221904fb
virche22 <v.tauto-an.564376@umindanao.edu.ph>
2026-05-08 09:25:31 +0800

    partial repo error

---
+++ backend/src/main/java/repository/constructionEquipmentTrackerRepository.java
@@ -0,0 +1,10 @@
+
+package teamBangan.competition.repository;
+
+import teamBangan.competition.model.constructionEquipmentTrackerModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface constructionEquipmentTrackerRepository extends JpaRepository<constructionEquipmentTrackerModel, Long> {
+}
