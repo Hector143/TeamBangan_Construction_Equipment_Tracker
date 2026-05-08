3fe876cd80c6800d82af50486af71555c6206e91
virche22 <v.tauto-an.564376@umindanao.edu.ph>
2026-05-08 10:48:54 +0800

    update

--- backend/src/main/java/teamBangan/competition/CompetitionApplication.java
+++ backend/src/main/java/teamBangan/competition/CompetitionApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-// This tells Spring to look in the teamBangan folder AND the root folder for your packages
-@ComponentScan(basePackages = {"teamBangan.competition.model", "controller", "repository"})
+@ComponentScan(basePackages = {"teamBangan.competition", "controller", "repository"})
 public class CompetitionApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CompetitionApplication.class, args);
     }
